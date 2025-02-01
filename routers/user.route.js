77ba60cee902360a13610f4849581ac10fe5c796
bhawana <bhawanajupitech03@gmail.com>
2025-02-01 17:33:08 +0530

    user

--- routers/user.route.js
+++ routers/user.route.js
@@ -1,7 +1,10 @@
 import express from "express";
 import {
+  forgetPassword,
   login,
+  resetOtpVerify,
   signup,
+  updatePassword,
   updateUserstatus,
   userList,
 } from "../controllers/user.controller.js";
@@ -11,4 +14,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.get("/view", userList);
 router.post("/update-status/:id", updateUserstatus);
+router.post("/forget-password", forgetPassword);
+router.post("/reset-otp", resetOtpVerify);
+router.post("/update-password/:id", updatePassword);
 export default router;
