3a36fc59322b6f5b226fee9eade1cfe9f462dde9
bhawana <bhawanajupitech03@gmail.com>
2025-02-07 18:52:50 +0530

    user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -215,10 +215,3 @@ export const updateUser = async (req, res, next) => {
     res.status(500).json({ message: "Internal Server Error", status: false });
   }
 };
-
-/*  
-absurd-> betuka ,observe-> nirikshan ,imitation-> nakal,at all-> bilkul bhi,cursing-> apsabdh, etiquette,courtesy-> shistachar, wider-> vyapak,willingly-> svechha se ,conciseness-> sanshipti  
-
-
-
-*/
